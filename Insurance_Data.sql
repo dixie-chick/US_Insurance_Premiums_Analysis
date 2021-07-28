831a058d200a92728bdfdc089c2639f326e31272
Matthew R. Serventi <Matt@Matthews-MacBook-Pro.local>
2021-07-28 14:06:30 -0700

    added early sql data file

---
+++ Insurance_Data.sql
@@ -0,0 +1,11 @@
+CREATE TABLE insurance_data (
+	age INT NOT NULL,
+    bmi FLOAT NOT NULL,
+    children VARCHAR NOT NULL,
+    region VARCHAR NOT NULL,
+    charges FLOAT(2) NOT NULL,
+	smoker_yes INT NOT NULL,
+    smoker_no INT NOT NULL,
+    sex_female INT NOT NULL,
+    sex_male INT NOT NULL
+);
