ce7e079d31221c54b56dcd2ee698e8f87226da25
bchoi07 <bogeun.choi7@gmail.com>
2021-08-14 22:57:30 -0500

    work on interactive webpage and organizational stuff

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Taken from https://github.com/krishnaik06/Deployment-flask/blob/master/app.py
 import numpy as np
+import pandas as pd
 from flask import Flask, request, jsonify, render_template
 import pickle
 
@@ -15,13 +16,50 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    age = int(request.form['age'])
+    height = int(request.form['height'])
+    weight = int(request.form['weight'])
+    children = int(request.form['children'])
+    smoker = request.form['smoker']
+    sex = request.form['sex']
 
-    output = round(prediction[0], 2)
+    smoker_yes = 0
+    smoker_no = 0
+    sex_female = 0
+    sex_male = 0
 
-    return render_template('index.html', prediction_text='Insurance charges should be $ {}'.format(output))
+    if sex == 'f':
+        sex_female = 1
+    else:
+        sex_male = 1
+
+    if smoker == 'yes':
+        smoker_yes = 1
+    else:
+        smoker_no = 1
+
+    bmi = (weight / height ** 2) * 703
+
+    input_variables = pd.DataFrame([[age, bmi, children, smoker_no, smoker_yes, sex_female, sex_male]],
+                                   columns=['age', 'bmi', 'children', 'smoker_no', 'smoker_yes', 'sex_female', 'sex_male'])
+    reshaped_input = np.array(input_variables.iloc[0]).reshape(1, -1)
+
+    prediction = model.predict(reshaped_input)[0]
+    output = round(prediction, 2)
+
+    #int_features = [int(x) for x in request.form.values()]
+    #final_features = [np.array(int_features)]
+    #prediction = model.predict(final_features)
+
+    status = ''
+    if output < 20000:
+        status = 'You are a good candidate'
+    else:
+        status = 'You might be higher-risk'
+
+    prediction_text = 'Predicted insurance charges: ${}'.format(output) + '\n {}'.format(status)
+
+    return render_template('results.html', prediction_text=prediction_text)
 
 # @app.route('/predict_api',methods=['POST'])
 # def predict_api():
--- model.py
+++ model.py
@@ -1,40 +1,28 @@
 # import dependencies
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 import pickle
-
 
 # read in the cleaned file
 insurance_df = pd.read_csv('insurance_cleaned.csv')
-insurance_df.head()
 
 # create X and y features
-X = insurance_df.drop(columns = "charges")
+X = insurance_df.drop(columns="charges")
 y = insurance_df["charges"]
 
-#split data into training and testing
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
-X_train.shape
+# split data into training and testing
+# X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 
 # Create Linear Regression Model
-from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 
 # fit/train data
-regressor.fit(X_train,y_train)
-
-# predict
-y_pred = regressor.predict(X_test)
-# print(y_pred.shape)
-
-# plot to check?
-#plt.scatter(y_test,y_pred)
+regressor.fit(X,y)
 
 # Saving model to disk
 pickle.dump(regressor, open('model.pkl','wb'))
 
 # Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[2, 9, 6]]))
+# model = pickle.load(open('model.pkl','rb'))
+# print(model.predict([[2, 9, 6]]))
