41db84b434f4d249727e04ac59684aa6be6a701b
bchoi07 <bogeun.choi7@gmail.com>
2021-08-15 21:23:42 -0500

    aesthetic changes to html/css files

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
+    # HTML form information
     age = int(request.form['age'])
     height = int(request.form['height'])
     weight = int(request.form['weight'])
@@ -23,11 +24,13 @@ def predict():
     smoker = request.form['smoker']
     sex = request.form['sex']
 
+    # Placeholder variables
     smoker_yes = 0
     smoker_no = 0
     sex_female = 0
     sex_male = 0
 
+    # Setting placeholder variables from HTML form info
     if sex == 'f':
         sex_female = 1
     else:
@@ -38,26 +41,26 @@ def predict():
     else:
         smoker_no = 1
 
+    # Calculate BMI from weight and height info
     bmi = (weight / height ** 2) * 703
 
+    # Shape form info into Pandas Dataframe/Numpy array for model input
     input_variables = pd.DataFrame([[age, bmi, children, smoker_no, smoker_yes, sex_female, sex_male]],
                                    columns=['age', 'bmi', 'children', 'smoker_no', 'smoker_yes', 'sex_female', 'sex_male'])
     reshaped_input = np.array(input_variables.iloc[0]).reshape(1, -1)
 
+    # Predicting charges using model
     prediction = model.predict(reshaped_input)[0]
     output = round(prediction, 2)
 
-    #int_features = [int(x) for x in request.form.values()]
-    #final_features = [np.array(int_features)]
-    #prediction = model.predict(final_features)
-
+    # Getting status message to print with results
     status = ''
     if output < 20000:
         status = 'You are a good candidate'
     else:
         status = 'You might be higher-risk'
 
-    prediction_text = 'Predicted insurance charges: ${}'.format(output) + '\n {}'.format(status)
+    prediction_text = 'Predicted insurance charges: ${}'.format(output) + '\n' + '{}'.format(status)
 
     return render_template('results.html', prediction_text=prediction_text)
 
